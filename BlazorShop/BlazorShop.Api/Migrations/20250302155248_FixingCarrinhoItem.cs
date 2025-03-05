2d62054270438f90eebc7d1672fb36faf3266afe
Alves, Leonardo <Leonardo.Alves@sovos.com>
2025-03-05 00:04:22 -0300

    Initializing repo

---
+++ BlazorShop/BlazorShop.Api/Migrations/20250302155248_FixingCarrinhoItem.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorShop.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixingCarrinhoItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "QuantidadeId",
+                table: "CarrinhoItens",
+                newName: "Quantidade");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Quantidade",
+                table: "CarrinhoItens",
+                newName: "QuantidadeId");
+        }
+    }
+}
